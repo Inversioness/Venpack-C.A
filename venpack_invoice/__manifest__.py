21ab61d610dd8638ba08153bc7d54d77933197d6
Fabio Tamburini <fatm400@gmail.com>
2024-03-03 11:27:01 -0400

    fix(venpack_invoice): migrar facturas venpack a odoo 16 v5

--- venpack_invoice/__manifest__.py
+++ venpack_invoice/__manifest__.py
@@ -3,21 +3,20 @@
     'name': "Venpack Invoice",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Personalized invoices for venpack""",
 
     'description': """
-        Long description of module's purpose
+        Personalized invoices for venpack
     """,
 
-    'author': "Fabio Tamburini",
-    'website': "http://www.yourcompany.com",
+    'author': "Arkisoft / Fabio Tamburini",
+    'website': "",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Invoicing',
-    'version': '0.1',
+    'version': '16.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale', 'stock', 'tax_report'],
@@ -43,8 +42,10 @@
         'reports/municipal_withholding.xml',
         'wizard/report_municipal_withholding.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+
+    # 'assets': {
+    #     'web.assets_backend': [
+    #         'venpack_invoice/static/src/css/custom_css_reports.css'
+    #     ],
+    # }
 }
