86d172ea9807aec6fde49cb9d1a436797c53ee75
Fabio Tamburini <fatm400@gmail.com>
2025-05-12 15:41:47 -0400

    feat(venpack_invoice): agregar datos del producto en ordenes de ventas

--- venpack_invoice/models/sale_order.py
+++ venpack_invoice/models/sale_order.py
@@ -12,6 +12,12 @@ class CustomSaleOrder(models.Model):
     x_totalkd = fields.Float(string="Total Kilos Despachados", compute="_compute_totalkd", store=True, default=0)
     x_totaluds_despachadas = fields.Float(string="Total Unidades Despachadas", compute="_compute_totalusd_despachadas", store=True, default=0)
 
+    product_id = fields.Many2one('product.product', string="Nombre del Producto", compute="_compute_first_line_fields", store=True)
+    x_presentacion = fields.Char(string="Presentación", compute="_compute_first_line_fields", store=True)
+    uom_id = fields.Many2one('uom.uom', string="Unidad de Medida", compute="_compute_first_line_fields", store=True)
+    x_tipop = fields.Char(string="Tipo de Producto", compute="_compute_first_line_fields", store=True)
+    x_tipox = fields.Char(string="Tipo de Extrusión", compute="_compute_first_line_fields", store=True)
+
     @api.depends('x_peso', 'x_totaldespachado')
     def _compute_totalkd(self):
         for order in self:
@@ -27,3 +33,20 @@ class CustomSaleOrder(models.Model):
                 order.x_totaluds_despachadas = order.x_Totaluds - order.x_udsdespachadasestimado
             else:
                 order.x_totaluds_despachadas = 0
+
+    @api.depends('order_line')
+    def _compute_first_line_fields(self):
+        for order in self:
+            first_line = order.order_line[:1]
+            if first_line:
+                order.product_id = first_line.product_id
+                order.x_presentacion = dict(first_line._fields['x_presentacion'].selection).get(first_line.x_presentacion, '') if first_line.x_presentacion else ''
+                order.uom_id = first_line.product_uom
+                order.x_tipop = dict(first_line._fields['x_tipop'].selection).get(first_line.x_tipop, '') if first_line.x_tipop else ''
+                order.x_tipox = dict(first_line._fields['x_tipox'].selection).get(first_line.x_tipox, '') if first_line.x_tipox else ''
+            else:
+                order.product_id = False
+                order.x_presentacion = ''
+                order.uom_id = False
+                order.x_tipop = ''
+                order.x_tipox = ''
