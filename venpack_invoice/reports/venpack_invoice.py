b1efea7e2dd98e789cec06c02a6cf645ea9c86a2
Fabio Tamburini <fatm400@gmail.com>
2025-09-17 15:51:13 -0400

    fix(venpack_invoice): calculo de monsos en bs para factura en usd factura v1

--- venpack_invoice/reports/venpack_invoice.py
+++ venpack_invoice/reports/venpack_invoice.py
@@ -32,6 +32,22 @@ class PalmaSuitesInvoice(models.AbstractModel):
         amount_total = 0.0
         discount_sum = 0.0
 
+        # Cálculo de variables en ambas monedas: _ves (bolívares) y _rate (USD)
+        tasa = docs.x_tasa if docs.x_tasa else 0
+        # Inicialización
+        amount_untaxed_ves = 0.0
+        discount_sum_ves = 0.0
+        tax_iva_ves = 0.0
+        tax_base_ves = 0.0
+        exempt_sum_ves = 0.0
+        amount_total_ves = 0.0
+        amount_untaxed_rate = 0.0
+        discount_sum_rate = 0.0
+        tax_iva_rate = 0.0
+        tax_base_rate = 0.0
+        exempt_sum_rate = 0.0
+        amount_total_rate = 0.0
+
         street = docs.partner_id.street if docs.partner_id.street else ''
         street2 = docs.partner_id.street2 if docs.partner_id.street2 else ''
         zip_code = docs.partner_id.zip if docs.partner_id.zip else ''
@@ -78,6 +94,8 @@ class PalmaSuitesInvoice(models.AbstractModel):
                     else:
                         iva_withheld = line_riva_id.credit
 
+        
+
         amount_total = tax_iva + tax_base + exempt_sum
 
         if percentage != '':
@@ -86,18 +104,79 @@ class PalmaSuitesInvoice(models.AbstractModel):
             retention_percentage = ''
 
 
+        # Cálculo de variables en ambas monedas: _ves (bolívares) y _rate (USD)
+        tasa = docs.x_tasa if docs.x_tasa else 0
+        # Inicialización
+        amount_untaxed_ves = 0.0
+        discount_sum_ves = 0.0
+        tax_iva_ves = 0.0
+        tax_base_ves = 0.0
+        exempt_sum_ves = 0.0
+        amount_total_ves = 0.0
+        amount_untaxed_rate = 0.0
+        discount_sum_rate = 0.0
+        tax_iva_rate = 0.0
+        tax_base_rate = 0.0
+        exempt_sum_rate = 0.0
+        amount_total_rate = 0.0
+
+        if docs.currency_id.name == 'VES':
+            # Las variables _ves son las calculadas, las _rate se obtienen dividiendo entre la tasa
+            amount_untaxed_ves = amount_untaxed
+            discount_sum_ves = discount_sum
+            tax_iva_ves = tax_iva
+            tax_base_ves = tax_base
+            exempt_sum_ves = exempt_sum
+            amount_total_ves = amount_total
+
+            amount_untaxed_rate = amount_untaxed / tasa if tasa else 0
+            discount_sum_rate = discount_sum / tasa if tasa else 0
+            tax_iva_rate = tax_iva / tasa if tasa else 0
+            tax_base_rate = tax_base / tasa if tasa else 0
+            exempt_sum_rate = exempt_sum / tasa if tasa else 0
+            amount_total_rate = amount_total / tasa if tasa else 0
+        else:
+            # amount_total = tax_iva + tax_base + exempt_sum
+            # Las variables _rate son las calculadas, las _ves se obtienen multiplicando por la tasa
+            amount_untaxed_rate = amount_untaxed
+            discount_sum_rate = discount_sum
+            tax_base_rate = tax_base
+            exempt_sum_rate = exempt_sum
+            tax_iva_rate = tax_iva
+            amount_total_rate = amount_total
+
+            amount_untaxed_ves = amount_untaxed * tasa if tasa else 0
+            discount_sum_ves = discount_sum * tasa if tasa else 0
+            tax_base_ves = amount_untaxed_ves
+            exempt_sum_ves = exempt_sum * tasa if tasa else 0
+            # IVA en VES: 16% del monto base en VES
+            tax_iva_ves = tax_base_ves * 0.16
+            amount_total_ves = amount_untaxed_ves + tax_iva_ves + exempt_sum_ves
+
         docargs = {
             'doc_ids': docids,
             'doc_model': 'account.move',
             'data': data,
             'docs': docs,
-            'amount_untaxed': docs.amount_untaxed,
+            'amount_untaxed': amount_untaxed,
             'discount_sum': discount_sum,
             'tax_iva': tax_iva,
             'exempt_sum': exempt_sum,
             'amount_total': amount_total,
             'lines': lines,
             'address': address,
-
+            # Variables para la otra moneda
+            'amount_untaxed_ves': amount_untaxed_ves,
+            'discount_sum_ves': discount_sum_ves,
+            'tax_iva_ves': tax_iva_ves,
+            'tax_base_ves': tax_base_ves,
+            'exempt_sum_ves': exempt_sum_ves,
+            'amount_total_ves': amount_total_ves,
+            'amount_untaxed_rate': amount_untaxed_rate,
+            'discount_sum_rate': discount_sum_rate,
+            'tax_iva_rate': tax_iva_rate,
+            'tax_base_rate': tax_base_rate,
+            'exempt_sum_rate': exempt_sum_rate,
+            'amount_total_rate': amount_total_rate,
         }
         return docargs
